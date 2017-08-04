9cad1406b5dfd399d21bcd8db40cca61431c30d2
mikey.zhaopeng <imzhpe@qq.com>
2017-08-04 16:40:52 +0800

    add gpu

--- 02-Logistic Regression/LogisticRegression.py
+++ 02-Logistic Regression/LogisticRegression.py
@@ -1,8 +1,18 @@
-import  torch
+import torch
 
-x = torch.rand(3,3)
+import numpy as np
 
-print(x)
+d = np.array([
+    [1, 34, 88,333],
+    [6, 42, 5,4],
+    [253, 34, 1,2]
+])
 
-print(torch.max(x,1))# max(data,dim) dim 取值是从-2到1,取某个维度的最大值
-print(torch.max(x,0))
+x = torch.from_numpy(d)
+a, b = torch.max(x, 1);
+
+print(a)
+print(b)# b代表从哪个位子的取值
+#
+# print(torch.max(x,1))# max(data,dim) dim 取值是从-2到1,取某个维度的最大值
+# print(torch.max(x,0))
