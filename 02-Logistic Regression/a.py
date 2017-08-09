429cf582dd85248e595e2acd41ab0dd6a5a3ab1d
mikey.zhaopeng <imzhpe@qq.com>
2017-08-09 17:51:45 +0800

    add gpu

--- 02-Logistic Regression/a.py
+++ 02-Logistic Regression/a.py
@@ -9,23 +9,38 @@ from torchvision import transforms
 from torchvision import datasets
 
 # 定义超参数
-batch_size = 32 # 每次拿出来多少个样本进行训练
+batch_size = 32  # 每次拿出来多少个样本进行训练
 learning_rate = 1e-3  # +1e-3表示1*10^-3，即0.001    e的负3次方是exp（3）
 
-num_epoches = 100
+num_epoches = 100  # x训练次数
+
+# mnist 的数据结构 http://blog.csdn.net/qq_32166627/article/details/62218072  在这个里面有说明,
+# pytorch 已经对这些数据做了处理
 
 # 下载训练集 MNIST 手写数字训练集
 train_dataset = datasets.MNIST(
-    root='./data', train=True, transform=transforms.ToTensor(), download=True)
+    root='./data',  # 数据保存的路径
+    train=True,  # 是否为训练数据还是测试数据
+    transform=transforms.ToTensor(),  # 把数据转化为 tensor 格式,便于数据使用存储
+    download=True  # 是否下载,如果已经下载完成,会自动跳过
+)
 
 test_dataset = datasets.MNIST(
     root='./data', train=False, transform=transforms.ToTensor())
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+# DataLoader 批量数据训练的包装类
+train_loader = DataLoader(
+    train_dataset,  # 处理使用的数据集
+    batch_size=batch_size,  # 每次batch 加载的样本数量
+    num_workers=2,  # 加载数据的使用几个线程
+    shuffle=True # 每次训练取数据的时候,是否随机打乱顺序
+)
+
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 
 # 定义 Logistic Regression 模型
+# 定义逻辑回归模型
 class Logstic_Regression(nn.Module):
     def __init__(self, in_dim, n_class):
         super(Logstic_Regression, self).__init__()
@@ -35,14 +50,16 @@ class Logstic_Regression(nn.Module):
         out = self.logstic(x)
         return out
 
+# in_dim 数据的维度
+# n_class 分类数量
 
 model = Logstic_Regression(28 * 28, 10)  # 图片大小是28x28
 use_gpu = torch.cuda.is_available()  # 判断是否有GPU加速
 if use_gpu:
     model = model.cuda()
 # 定义loss和optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+criterion = nn.CrossEntropyLoss() # 交叉熵
+optimizer = optim.SGD(model.parameters(), lr=learning_rate) # 随机梯度下降
 
 # 开始训练
 for epoch in range(num_epoches):
@@ -71,8 +88,6 @@ for epoch in range(num_epoches):
         loss.backward()
         optimizer.step()
 
-
-
         if i % 300 == 0:
             print('[{}/{}] Loss: {:.6f}, Acc: {:.6f}'.format(
                 epoch + 1, num_epoches, running_loss / (batch_size * i),
@@ -81,10 +96,7 @@ for epoch in range(num_epoches):
         epoch + 1, running_loss / (len(train_dataset)), running_acc / (len(
             train_dataset))))
 
-
     model.eval()
-
-
 
     eval_loss = 0.
     eval_acc = 0.
--- other/test.py
+++ other/test.py
@@ -1,37 +1,37 @@
 import torch
 import numpy as np
-
-torch.manual_seed(12345)# 手动设置随机因子,保证随机出来的结果一致
-x = torch.rand(3,1)
-print(x)
-
-y = torch.rand(3,1)
-print(y)
-print(x==y)
-
-x = np.array([[3,34,39],[1,345,64],[2,456,41],[56,3453,23]])
-print(x)
-x = torch.from_numpy(x)
-print(x)
-
-
-y = np.array([[43],[23],[2],[56]])
-print(y)
-y = torch.from_numpy(y)
-print(y)
-
-
-print(x == y)
+#
+# torch.manual_seed(12345)# 手动设置随机因子,保证随机出来的结果一致
+# x = torch.rand(3,1)
+# print(x)
+#
+# y = torch.rand(3,1)
+# print(y)
+# print(x==y)
+#
+# x = np.array([[3,34,39],[1,345,64],[2,456,41],[56,3453,23]])
+# print(x)
+# x = torch.from_numpy(x)
+# print(x)
+#
+#
+# y = np.array([[43],[23],[2],[56]])
+# print(y)
+# y = torch.from_numpy(y)
+# print(y)
+#
+#
+# print(x == y)
 
 
 print('00000000000000000000000000000')
 
-a = torch.arange(1, 16)
+a = torch.arange(1, 17)
 
 print(a)
-a = a.view(4, -1)
+a = a.view(6, -1)
 print(a)
 
-a = a.view(4, 4)
+# a = a.view(4, 4)
 
 # print(a)
