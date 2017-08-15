78a1efe72c592816b779ff4b54836865b88a4316
mikey.zhaopeng <imzhpe@qq.com>
2017-08-15 17:07:29 +0800

    add comment

--- 04-Convolutional Neural Network/b.py
+++ 04-Convolutional Neural Network/b.py
@@ -4,27 +4,31 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
+import numpy as np
 
 import time
 
-batch_size = 32
-learning_rate = 0.01
-num_epoches = 50
-use_gpu = torch.cuda.is_available()
+# batch_size = 32
+# learning_rate = 0.01
+# num_epoches = 50
+# use_gpu = torch.cuda.is_available()
+#
+# train_dataset = datasets.MNIST(
+#     root='./data',
+#     train=True,
+#     transform=transforms.ToTensor(),
+#     download=True
+# )
+#
+# test_dataset = datasets.MNIST(
+#     root='./data',
+#     train=False,
+#     transform=transforms.ToTensor(),
+#     download=True
+# )
 
-train_dataset = datasets.MNIST(
-    root='./data',
-    train=True,
-    transform=transforms.ToTensor(),
-    download=True
-)
 
-test_dataset = datasets.MNIST(
-    root='./data',
-    train=False,
-    transform=transforms.ToTensor(),
-    download=True
-)
-
-print(test_dataset)
-
+x = np.random.rand(3,3)
+print(x)
+print('*' *20)
+print(x[-1])
